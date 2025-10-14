e3e581fab7d84a559b812b7839f71246e3a42f9f
Bunthet SAY <bunthet.say@presta.tessi.fr>
2025-10-14 10:12:33 +0200

    init product service

---
+++ product-service/src/test/java/io/github/sbunthet/productservice/ProductServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package io.github.sbunthet.productservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProductServiceApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
